fd2646a7d296c71e7d156eac6b478bc62e8da974
Sander van Leeuwen <djsavale@hotmail.com>
2014-07-02 11:23:31 +0200

    Update bitblock.c

--- bitblock.c
+++ bitblock.c
@@ -24,6 +24,14 @@ void bitblock_hash(const char* input, char* output)
     sph_simd512_init (&ctx_simd1); 
     sph_simd512 (&ctx_simd1, hashA, 64);   
     sph_simd512_close(&ctx_simd1, hashB); 
+    
+    sph_shavite512_init (&ctx_shavite1);
+    sph_shavite512 (&ctx_shavite1, hashB, 64);   
+    sph_shavite512_close(&ctx_shavite1, hashA);  
+	
+    sph_simd512_init (&ctx_simd1); 
+    sph_simd512 (&ctx_simd1, hashA, 64);   
+    sph_simd512_close(&ctx_simd1, hashB); 
 	
     sph_echo512_init (&ctx_echo1); 
     sph_echo512 (&ctx_echo1, hashB, 64);   
